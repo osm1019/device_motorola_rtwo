3ad31b6191454032d5b04fdc1637b8067090fd17
Marc Bourgoin <themard85@gmail.com>
2024-04-11 18:20:00 -0600

    rtwo: Update blobs from rtwo_g-user 14 U1TRS34.8-30-13-1 7c77a-12bae6 release-keys - RETAIL variant
    Change-Id: I7980db1b41e79846dbe5671c2ed274c9e8f6983f

--- lineage_rtwo.mk
+++ lineage_rtwo.mk
@@ -24,6 +24,6 @@ PRODUCT_GMS_CLIENTID_BASE := android-motorola
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_PRODUCT=rtwo_g \
-    PRIVATE_BUILD_DESC="rtwo_g-user 14 U1TR34.8-30-13 c6cfc-4ed033 release-keys"
+    PRIVATE_BUILD_DESC="rtwo_g-user 14 U1TRS34.8-30-13-1 7c77a-12bae6 release-keys"
 
-BUILD_FINGERPRINT := motorola/rtwo_g/msi:14/U1TR34.8-30-13/c6cfc-4ed033:user/release-keys
+BUILD_FINGERPRINT := motorola/rtwo_g/rtwo:14/U1TRS34.8-30-13-1/7c77a-12bae6:user/release-keys
