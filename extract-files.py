6c04fa20efcb0ff12bf6d9138694013a429c404a
Michael Bestas <mkbestas@lineageos.org>
2024-11-06 14:07:20 +0200

    rtwo: ruff format --config
    Change-Id: I134c16aaa825feab7aee7057f524818f58603681

--- extract-files.py
+++ extract-files.py
@@ -55,5 +55,7 @@ module = ExtractUtilsModule(
 )
 
 if __name__ == '__main__':
-    utils = ExtractUtils.device_with_common(module, 'sm8550-common', module.vendor)
+    utils = ExtractUtils.device_with_common(
+        module, 'sm8550-common', module.vendor
+    )
     utils.run()
