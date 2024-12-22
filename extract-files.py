motorola-sm8550/android_device_motorola_rtwo

rtwo: Switch to python extract-utils
Change-Id: Ida4b83459d9739ac880b26a91df974e1db8423e9

extract-files.py
@@ -1,0 +1,60 @@
+#!/usr/bin/env -S PYTHONPATH=../../../tools/extract-utils python3
+#
+# SPDX-FileCopyrightText: 2024 The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+from extract_utils.extract import extract_fns_user_type
+from extract_utils.extract_star import extract_star_firmware
+from extract_utils.fixups_blob import (
+    blob_fixup,
+    blob_fixups_user_type,
+)
+from extract_utils.fixups_lib import (
+    lib_fixup_vendorcompat,
+    lib_fixups_user_type,
+    libs_proto_3_9_1,
+)
+from extract_utils.main import (
+    ExtractUtils,
+    ExtractUtilsModule,
+)
+
+namespace_imports = [
+    'vendor/motorola/sm8550-common',
+    'hardware/qcom-caf/sm8350',
+    'hardware/qcom-caf/wlan',
+    'hardware/motorola',
+    'vendor/qcom/opensource/commonsys-intf/display',
+    'vendor/qcom/opensource/commonsys/display',
+    'vendor/qcom/opensource/dataservices',
+    'vendor/qcom/opensource/display',
+]
+
+lib_fixups: lib_fixups_user_type = {
+    libs_proto_3_9_1: lib_fixup_vendorcompat,
+}
+
+blob_fixups: blob_fixups_user_type = {
+    'product/priv-app/MotCamera4/MotCamera4.apk': blob_fixup()
+        .apktool_patch('MotCamera4-patches'),
+}  # fmt: skip
+
+extract_fns: extract_fns_user_type = {
+    r'(bootloader|radio)\.img': extract_star_firmware,
+}
+
+module = ExtractUtilsModule(
+    'rtwo',
+    'motorola',
+    blob_fixups=blob_fixups,
+    lib_fixups=lib_fixups,
+    namespace_imports=namespace_imports,
+    extract_fns=extract_fns,
+    add_firmware_proprietary_file=True,
+    add_generated_carriersettings=True,
+)
+
+if __name__ == '__main__':
+    utils = ExtractUtils.device_with_common(module, 'sm8550-common', module.vendor)
+    utils.run()

osm1019/device_motorola_rtwo

extract-files.py
@@ -1,0 +1,60 @@
+#!/usr/bin/env -S PYTHONPATH=../../../tools/extract-utils python3
+#
+# SPDX-FileCopyrightText: 2024 The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+from extract_utils.extract import extract_fns_user_type
+from extract_utils.extract_star import extract_star_firmware
+from extract_utils.fixups_blob import (
+    blob_fixup,
+    blob_fixups_user_type,
+)
+from extract_utils.fixups_lib import (
+    lib_fixup_vendorcompat,
+    lib_fixups_user_type,
+    libs_proto_3_9_1,
+)
+from extract_utils.main import (
+    ExtractUtils,
+    ExtractUtilsModule,
+)
+
+namespace_imports = [
+    'vendor/motorola/sm8550-common',
+    'hardware/qcom-caf/sm8550',
+    'hardware/qcom-caf/wlan',
+    'hardware/motorola',
+    'vendor/qcom/opensource/commonsys-intf/display',
+    'vendor/qcom/opensource/commonsys/display',
+    'vendor/qcom/opensource/dataservices',
+    'vendor/qcom/opensource/display',
+]
+
+lib_fixups: lib_fixups_user_type = {
+    libs_proto_3_9_1: lib_fixup_vendorcompat,
+}
+
+blob_fixups: blob_fixups_user_type = {
+    'product/priv-app/MotCamera4/MotCamera4.apk': blob_fixup()
+        .apktool_patch('MotCamera4-patches'),
+}  # fmt: skip
+
+extract_fns: extract_fns_user_type = {
+    r'(bootloader|radio)\.img': extract_star_firmware,
+}
+
+module = ExtractUtilsModule(
+    'rtwo',
+    'motorola',
+    blob_fixups=blob_fixups,
+    lib_fixups=lib_fixups,
+    namespace_imports=namespace_imports,
+    extract_fns=extract_fns,
+    add_firmware_proprietary_file=True,
+    add_generated_carriersettings=True,
+)
+
+if __name__ == '__main__':
+    utils = ExtractUtils.device_with_common(module, 'sm8550-common', module.vendor)
+    utils.run()